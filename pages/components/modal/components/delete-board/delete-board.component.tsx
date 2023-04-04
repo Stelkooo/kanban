a85af8d046afafb33236d473e88e1013b1bd2a22
stelko <stilyanmtoskov@gmail.com>
2023-04-04 11:53:00 +0100

    delete task and board modal

---
+++ pages/components/modal/components/delete-board/delete-board.component.tsx
@@ -0,0 +1,26 @@
+import { TBoard } from '@/pages/types/kanban.types';
+
+import Button from '@/pages/components/button/button.component';
+import Modal from '../../modal.component';
+
+type Props = { board: TBoard };
+
+export default function DeleteBoard({ board }: Props) {
+  return (
+    <Modal>
+      <h3 className="heading-large text-red">Delete this board?</h3>
+      <p className="body-large text-medium-grey">
+        Are you sure you want to delete the ‘{board.name}’ board? This action
+        will remove all columns and tasks and cannot be reversed.
+      </p>
+      <div className="flex flex-col gap-4">
+        <Button btnStyle="destructive">
+          <p className="body-medium">Delete</p>
+        </Button>
+        <Button btnStyle="secondary">
+          <p className="body-medium">Cancel</p>
+        </Button>
+      </div>
+    </Modal>
+  );
+}
---
+++ pages/components/modal/components/delete-task/delete-task.component.tsx
@@ -0,0 +1,26 @@
+import { TTask } from '@/pages/types/kanban.types';
+
+import Button from '@/pages/components/button/button.component';
+import Modal from '../../modal.component';
+
+type Props = { task: TTask };
+
+export default function DeleteTask({ task }: Props) {
+  return (
+    <Modal>
+      <h3 className="heading-large text-red">Delete this task?</h3>
+      <p className="body-large text-medium-grey">
+        Are you sure you want to delete the ‘{task.title}’ task and its
+        subtasks? This action cannot be reversed.
+      </p>
+      <div className="flex flex-col gap-4">
+        <Button btnStyle="destructive">
+          <p className="body-medium">Delete</p>
+        </Button>
+        <Button btnStyle="secondary">
+          <p className="body-medium">Cancel</p>
+        </Button>
+      </div>
+    </Modal>
+  );
+}
