8d1c61a836ee6c9b7f3f80d80e849d5faa3d54e8
stelko <stilyanmtoskov@gmail.com>
2023-04-04 11:32:24 +0100

    add board modal

---
+++ pages/components/modal/components/add-board/add-board.component.tsx
@@ -0,0 +1,29 @@
+import { TBoard } from '@/pages/types/kanban.types';
+import Button from '@/pages/components/button/button.component';
+import Modal from '../../modal.component';
+import Columns from './columns/columns.component';
+
+type Props = {
+  board: TBoard;
+};
+
+export default function AddBoard({ board }: Props) {
+  return (
+    <Modal>
+      <h3 className="heading-large">Add New Board</h3>
+      <label htmlFor="title" className="grid gap-y-2">
+        <p className="body-medium text-medium-grey">Name</p>
+        <input
+          type="text"
+          id="title"
+          className="body-large rounded-[4px] border border-lines-light px-4 py-2"
+          placeholder="e.g. Take coffee break"
+        />
+      </label>
+      <Columns />
+      <Button btnStyle="primarySmall">
+        <p className="body-medium">Create New Board</p>
+      </Button>
+    </Modal>
+  );
+}
