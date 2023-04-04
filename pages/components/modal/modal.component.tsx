93d33a770ae9b97ca15bc18236172a9a70acfefe
stelko <stilyanmtoskov@gmail.com>
2023-04-04 20:21:25 +0100

    edit board modal

--- pages/components/modal/modal.component.tsx
+++ pages/components/modal/modal.component.tsx
@@ -1,26 +1,26 @@
 'use client';
 
 import { useAppSelector } from '@/store/hooks';
-import {
-  selectModalType,
-  selectTask,
-  selectBoard,
-} from '@/store/modal/modal.selector';
+import { selectModalType, selectTask } from '@/store/modal/modal.selector';
+import { selectCurrentBoard } from '@/store/kanban/kanban.selector';
+
 import ViewTask from './components/view-task/view-task.component';
 import AddTask from './components/add-task/add-task.component';
 import EditTask from './components/edit-task/edit-task.component';
 import ModalTemplate from './components/modal-template/modal-template.component';
 import AddBoard from './components/add-board/add-board.component';
+import EditBoard from './components/edit-board/edit-board.component';
 
 export default function Modal() {
   const modalType = useAppSelector(selectModalType);
   const task = useAppSelector(selectTask);
-  const board = useAppSelector(selectBoard);
+  const board = useAppSelector(selectCurrentBoard);
 
   if (modalType === 'view-task' && task) return <ViewTask task={task} />;
   if (modalType === 'add-task') return <AddTask />;
   if (modalType === 'edit-task' && task) return <EditTask task={task} />;
   if (modalType === 'add-board') return <AddBoard />;
+  if (modalType === 'edit-board' && board) return <EditBoard board={board} />;
   return (
     <ModalTemplate>
       <p>Modal does not exist :/</p>
