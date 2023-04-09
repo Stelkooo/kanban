f68112495eaa846420c9f149a81656bfe939457e
stelko <stilyanmtoskov@gmail.com>
2023-04-09 23:00:07 +0100

    add new column onclick func

--- src/sections/main/add-new-column/add-new-column.component.tsx
+++ src/sections/main/add-new-column/add-new-column.component.tsx
@@ -1,7 +1,37 @@
+import { useRouter } from 'next/router';
+
+import { useAppDispatch } from '@/store/hooks';
+import {
+  setModalToggle,
+  setModalType,
+  setBoard,
+} from '@/store/modal/modal.reducer';
+
+import { boardApi } from '@/store/api/api.store';
+
 export default function AddNewColumn() {
+  const router = useRouter();
+
+  const dispatch = useAppDispatch();
+
+  const { data } = boardApi.endpoints.getBoard.useQueryState(
+    router.query.board as string
+  );
+
+  const onClickHandler = () => {
+    if (data) {
+      dispatch(setBoard(data));
+      dispatch(setModalType('edit-board'));
+      dispatch(setModalToggle());
+    }
+  };
   return (
-    <div className="mt-10 grid h-full min-w-[280px] place-content-center rounded-md bg-lines-light">
+    <button
+      type="button"
+      className="mt-10 grid h-full min-w-[280px] place-content-center rounded-md bg-lines-light"
+      onClick={() => onClickHandler()}
+    >
       <h2 className="heading-large text-medium-grey">New Column</h2>
-    </div>
+    </button>
   );
 }
